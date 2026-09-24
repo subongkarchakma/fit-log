de25e707adbf00496cfdf93591304c66b546a3af
subongkarchakmama <subongkar.chakma@gmail.com>
2026-09-24 23:35:13 +0600

    Navbar added and modified

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -4,7 +4,7 @@ import logo from "@/assets/logo.png";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm rounded-lg">
+        <div className="navbar bg-base-100 shadow-sm container mx-auto rounded-lg">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
